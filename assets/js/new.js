6c2a1a05f2df72dd5edebe0badbfe90762f7f989
jadhavrahulrj01 <jadhavrahulrj01@gmail.com>
2023-07-26 15:46:00 +0530

    many changes

--- assets/js/new.js
+++ assets/js/new.js
@@ -1,8 +1,1 @@
-// var a = document.getElementById("statusShow");
-// function show(){
-//     a.classList.toggle("show");
-// }
-
-// function clos(){
-//     a.classList.toggle("close");
-// }
+// new js
